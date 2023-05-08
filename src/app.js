523030ee97b05fa908a26a42d54ef99f1c0197cb
Santiago Zarate <santiago2001zarate@gmail.com>
2023-05-07 23:24:22 -0300

    Pagina inicial casi terminada, faltan medias querys

--- src/app.js
+++ src/app.js
@@ -7,7 +7,10 @@ const saboresElement = document.querySelectorAll("[sabores]");
 window.addEventListener("scroll", cambiarHeader);
 
 saboresElement.forEach(element => {
-    element.addEventListener("click", expandirInformacion(element));    
+    element.addEventListener("click", function(){
+        expandirInformacion(element);
+        animarCruz(element);
+    })
 });
 
 // FUNCIONES
@@ -19,4 +22,10 @@ function cambiarHeader(){
 function expandirInformacion(e){
     const boxExpandibleElement = e.children[1];
     boxExpandibleElement.classList.toggle("open-box");
+}
+
+function animarCruz(e){
+    const boxConLaCruz = e.children[0].children[1];
+    boxConLaCruz.classList.toggle("girar-plus-abrir");
+    boxConLaCruz.classList.toggle("girar-plus-cerrar");
 }
